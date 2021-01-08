14c644f9c017c078c67e489373722d0e2ec9394b
Kim Walisch <kim.walisch@gmail.com>
2021-01-08 12:59:26 +0100

    Revert to v6.2 (still no optimization flags)

--- Formula/primecount.rb
+++ Formula/primecount.rb
@@ -1,18 +1,19 @@
 class Primecount < Formula
   desc "Fast prime counting function program and C/C++ library"
   homepage "https://github.com/kimwalisch/primecount"
-  url "https://github.com/kimwalisch/primecount/archive/v6.1.tar.gz"
-  sha256 "222bb0a17ad31f09296e2f3318a367230bcda2b82bec2613e339ba5c5e8558a0"
+  url "https://github.com/kimwalisch/primecount/archive/v6.2.tar.gz"
+  sha256 "51626a8b2500eb30cba171e1358f3eb589552c8a672babd4eb25e0c9ac054091"
   license "BSD-2-Clause"
 
   depends_on "cmake" => :build
   depends_on "llvm" => :build
   depends_on "libomp"
+  depends_on "primesieve"
 
   def install
     mkdir "build" do
       # Build primecount using non-default LLVM compiler with libomp (OpenMP)
-      system "cmake", "..", "-DCMAKE_CXX_COMPILER=" + Formula["llvm"].bin + "/clang++", "-DBUILD_SHARED_LIBS=ON", *std_cmake_args
+      system "cmake", "..", "-DCMAKE_CXX_COMPILER=" + Formula["llvm"].bin + "/clang++", "-DBUILD_SHARED_LIBS=ON", "-DBUILD_LIBPRIMESIEVE=OFF", *std_cmake_args
       system "make", "install"
     end
   end
