1fb5b8f1d609e34338f50d29c61bb749a9e4c11c
Kim Walisch <kim.walisch@gmail.com>
2021-02-25 13:56:09 +0100

    Disable libdivide on Apple Silicon CPUs

--- Formula/primecount.rb
+++ Formula/primecount.rb
@@ -10,11 +10,22 @@ class Primecount < Formula
   depends_on "libomp"
   depends_on "primesieve"
 
+  # In 2021 integer division is slow on most CPUs,
+  # hence we use libdivide instead.
+  libdivide_build_option = "-DWITH_LIBDIVIDE=ON"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      # Apple Silicon CPUs have very fast integer division
+      libdivide_build_option = "-DWITH_LIBDIVIDE=OFF"
+    end
+  end
+  
   def install
     mkdir "build" do
       # 1) Build primecount using non-default LLVM compiler with libomp (OpenMP)
       # 2) Homebrew does not allow compiling with -O2 or -O3, instead homebrew requires using -Os
-      system "cmake", "..", "-DCMAKE_CXX_COMPILER=" + Formula["llvm"].bin + "/clang++", "-DCMAKE_CXX_FLAGS=-Os", "-DBUILD_SHARED_LIBS=ON", "-DBUILD_LIBPRIMESIEVE=OFF", *std_cmake_args
+      system "cmake", "..", "-DCMAKE_CXX_COMPILER=" + Formula["llvm"].bin + "/clang++", "-DCMAKE_CXX_FLAGS=-Os", "-DBUILD_SHARED_LIBS=ON", "-DBUILD_LIBPRIMESIEVE=OFF", libdivide_build_option, *std_cmake_args
       system "make", "install"
     end
   end
